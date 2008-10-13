a5399180ec2ad98184cbfd566ccc0f6a67fc6316
yogaskywill <yogaskywill@8415483c-3252-0410-a78a-13cb69a71b15>
2008-10-13 10:26:35 +0000

    搜索结果排序调整

--- app/helpers/searchings_helper.rb
+++ app/helpers/searchings_helper.rb
@@ -39,9 +39,9 @@ module SearchingsHelper
 		searchables_set = searchables_set_1 | searchables_set_2
 		
 		if limit
-			searchables_set[0..limit-1]
+			searchables_set[0..limit-1].sort! {|a,b| b[:created_at] <=> a[:created_at]}
 		else
-			searchables_set
+			searchables_set.sort! {|a,b| b[:created_at] <=> a[:created_at]}
 		end
 	end
 	
