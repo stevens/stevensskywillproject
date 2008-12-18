e5a034221220c6621cb38c54b10b2cb5fe87acfd
yogaskywill <yogaskywill@8415483c-3252-0410-a78a-13cb69a71b15>
2008-12-18 12:22:44 +0000

    修正多关键字搜索时搜到草稿的bug

--- app/helpers/searchings_helper.rb
+++ app/helpers/searchings_helper.rb
@@ -27,7 +27,7 @@ module SearchingsHelper
 	end
 	
 	def searchables_for(user = nil, searchable_type = nil, keywords = [], searchable_conditions = nil, exclude = false, match_all = false, limit = nil, order = 'created_at DESC')
-		conditions = searchable_type == 'User' ? [keywords_to_conditions(keywords, 'login')] : [keywords_to_conditions(keywords)]
+		conditions = searchable_type == 'User' ? ["(#{keywords_to_conditions(keywords, 'login')})"] : ["(#{keywords_to_conditions(keywords)})"]
 		conditions << "#{controller_name(searchable_type)}.user_id = #{user.id}" if user
 		conditions << searchable_conditions if searchable_conditions
 		
