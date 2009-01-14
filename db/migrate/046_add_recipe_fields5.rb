7a8404c96c99c613a469b72c2da44c3f475287dd
yogaskywill <yogaskywill@8415483c-3252-0410-a78a-13cb69a71b15>
2009-01-14 12:00:25 +0000

    为食谱增加常用名称

--- db/migrate/046_add_recipe_fields5.rb
+++ db/migrate/046_add_recipe_fields5.rb
@@ -3,11 +3,13 @@ class AddRecipeFields5 < ActiveRecord::Migration
   	add_column :recipes, :roles, :string
   	add_column :recipes, :original_updated_at, :datetime
   	add_column :recipes, :match_id, :integer
+  	add_column :recipes, :common_title, :string
   end
 
   def self.down
   	remove_column :recipes, :roles
   	remove_column :recipes, :original_updated_at
   	remove_column :recipes, :match_id
+  	remove_column :recipes, :common_title
   end
 end
