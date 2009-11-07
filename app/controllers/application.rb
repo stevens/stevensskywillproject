50abb310320f447f4f7da4158d1c0c420beaeeda
yogaskywill <yogaskywill@8415483c-3252-0410-a78a-13cb69a71b15>
2009-11-07 15:58:27 +0000

    [UI]更新网站的系统消息内容

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -130,7 +130,9 @@ class ApplicationController < ActionController::Base
 #    @system_notice = "<a href='#{url_for(:controller => 'matches', :action => 'profile', :id => 1)}'>金蜂·美食人生 大赛（第一季）</a> <em class='l3'><a href='#{url_for(:match_id => 1, :controller => 'winners', :action => 'index')}'>获奖名单</a></em> 和 <em class='l3'><a href='http://beecook2008.blogspot.com/2009/02/blog-post.html' target='_blank'>金蜂食单</a></em> 揭晓啦，恭喜获奖的作品和蜂友们！<br /><br />
 #                      蜂厨服务生已经给获奖的蜂友发出了奖品，请各位注意查收喔！"
 #    @system_notice = "<em class='l3'><a href='#{menus_path}'>餐单</a></em> 新鲜出炉啦！欢迎蜂友们抢先试用，快来跟大家分享你的美味餐单呵！"
-     @system_notice = "蜂厨慈善创意活动—— <em class='l3'><a href='http://blog.sina.com.cn/s/blog_5eb976840100eifm.html' target='_blank'>爱心食谱行动</a></em> 第一季进行中... 美味无敌快乐PK赛—— <em class='l3'><a href='http://blog.sina.com.cn/s/blog_5eb976840100f04b.html' target='_blank'>中西点心对对碰</a></em> 也在进行中..."
+#    @system_notice = "蜂厨慈善创意活动—— <em class='l3'><a href='http://blog.sina.com.cn/s/blog_5eb976840100eifm.html' target='_blank'>爱心食谱行动</a></em> 第一季进行中... 美味无敌快乐PK赛—— <em class='l3'><a href='http://blog.sina.com.cn/s/blog_5eb976840100f04b.html' target='_blank'>中西点心对对碰</a></em> 也在进行中..."
+     @system_notice = "蜂厨慈善创意活动 <em class='l3'><a href='http://blog.sina.com.cn/s/blog_5eb976840100eifm.html' target='_blank'>爱心食谱行动</a></em> 第一季火热进行中；特约合作伙伴 <em class='l3'><a href='http://spooon.taobao.com' target='_blank'>SPOOON</a></em> 带来源自丹麦的独创设计！<br /><br />
+                       新浪美食圈子 <em class='l2'><a href='http://q.blog.sina.com.cn/yryg' target='_blank'>生活绿皮书</a></em> 联手蜂厨即将推出 <em class='l2'><a href='http://blog.sina.com.cn/s/blog_4dd78ed30100fs5l.html' target='_blank'>《生活绿皮书》电子杂志感恩节特刊</a></em>，<a href='http://blog.sina.com.cn/s/blog_4dd78ed30100fs5l.html' target='_blank'>猛击这里查看详情</a>..."
 	end
 	
 	# def param_posted?(symbol)
